369e6f57140760ddf58e2a1583f6f400689a3810
Raul <raulnguyen91@gmail.com>
2016-06-01 02:48:11 +0200

    integration of SuperPoweredSDK with CrossExampleActivity

--- app/src/main/java/de/beuth/not1fy/app/MainActivity.java
+++ app/src/main/java/de/beuth/not1fy/app/MainActivity.java
@@ -1,11 +1,18 @@
 package de.beuth.not1fy.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +32,26 @@ public class MainActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch(item.getItemId()){
+            case R.id.action_settings:
+                Log.d("TAG","Button0");
+                break;
+            case R.id.action_cross_example:
+                Log.d("TAG","Button1");
+                Intent crossExampleIntent = new Intent(this, CrossExampleActivity.class);
+                this.startActivity(crossExampleIntent);
+                break;
+            case R.id.action_frequency_domain:
+                Log.d("TAG","Button2");
+                Intent frequenzDomainIntent = new Intent(this, FrequenzDomainActivity.class);
+                this.startActivity(frequenzDomainIntent);
+                break;
+            case R.id.action_hls_example:
+                Log.d("TAG","Button3");
+                Intent hlsExampleIntent = new Intent(this, HLSExampleActivity.class);
+                this.startActivity(hlsExampleIntent);
+                break;
         }
 
         return super.onOptionsItemSelected(item);
